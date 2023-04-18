d6c16b0ac63545d2e811bd9757436e159a269071
Mayconn Santos <mayconnbispo@gmail.com>
2023-04-18 13:51:03 -0300

    Ajusta url da requisição ao service webapp backend

--- frontend/js.js
+++ frontend/js.js
@@ -5,8 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
-        
+        url: "http://webapp-service/incluir.php",
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
         beforeSend: function() {
